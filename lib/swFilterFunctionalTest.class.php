5e386a6bc8bfbd86a4452da05c78637599d9676f
Thomas Rabaix <thomas.rabaix@soleoweb.com>
2010-02-02 15:01:25 +0000

    [swFunctionalTestGenerationPlugin] do not add _sw_func_link if the link content is empty
    git-svn-id: http://svn.symfony-project.com/plugins/swFunctionalTestGenerationPlugin/tags/VERSION_1_2_0@27419 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/swFilterFunctionalTest.class.php
+++ lib/swFilterFunctionalTest.class.php
@@ -53,7 +53,12 @@ class swFilterFunctionalTest extends sfFilter
         }
       } 
 
-      $link .= '_sw_func_link=' . $link_content;
+      // no content provided, ie: empty link
+      if(trim($link_content) != 0)
+      {
+        $link .= '_sw_func_link=' . $link_content;
+      }
+      
     }
 
     return sprintf('<a%shref=%s%s%s%s>%s</a>',
